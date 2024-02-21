d6bf7e87adb71a0c70786f7f1e783448c3c8ac1e
Chihoko Neu <chihoko.ueno@gmail.com>
2024-02-21 20:47:48 +0000

    serve an html file

--- myApp.js
+++ myApp.js
@@ -1,21 +1,13 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
+// console.log("Hello World");
 
-app.get("/",function(req,res) {
-    res.send('Hello Express');
+// app.get("/",function(req,res) {
+//     res.send('Hello Express');
+// });
+
+app.get("/", function(req,res) {
+    res.sendFile(__dirname + '/views/index.html');
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
  module.exports = app;
